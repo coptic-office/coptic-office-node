2e7c3ccb5b8922f19a65672594ae7027c22a578f
haseleye <haseleye@gmail.com>
2024-08-27 23:26:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -34,6 +34,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+})
 
 app.use('/system', sysRouter);
 app.use(checkAppLang);
