22e52207a4dee7bff1cd2d2da29a193d74bae59c
haseleye <haseleye@gmail.com>
2024-08-27 23:30:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
 })
 
 app.use('/system', sysRouter);
