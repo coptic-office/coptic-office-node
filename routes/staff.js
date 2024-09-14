59ae1a3089540af3c94f9f9691fdd17d0507b9ee
haseleye <haseleye@gmail.com>
2024-09-14 23:51:45 +0300

    Update staff.js

--- routes/staff.js
+++ routes/staff.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const staff = require('../controllers/staff');
 const validateMobile = require("../middleware/mobileValidation");
 const {authorize} = require("../middleware/auth");
+const Process = require("process");
+const multipartParser = require("../middleware/multipartParser");
 
 /** Staff login */
 router.post('/login', staff.login);
@@ -16,5 +18,11 @@ router.post('/verify-otp/', staff.verifyOTP);
 /** Change password using old password or verification code for those who forgot their passwords */
 const changePasswordRoles = ['Admin', 'EMPLOYEE', 'SUPERVISOR', 'MANAGER', 'Guest'];
 router.post('/change-password/', authorize('Access', changePasswordRoles), staff.changePassword);
+
+const fileTypesList = Process.env.IMAGE_FILE_TYPE.split(',');
+/** Update the staff profile photo */
+const updatePhotoRoles = ['Admin', 'EMPLOYEE', 'SUPERVISOR', 'MANAGER'];
+const options1 = {maxFileSize: 1, maxFilesCount: 1, fileTypesList};
+router.post('/update-photo', [authorize('Access', updatePhotoRoles), multipartParser(options1, 'image')], staff.updatePhoto);
 
 module.exports = router;
