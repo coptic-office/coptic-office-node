5f7690f75a5974fba496bcdac7a41d54b8b86767
haseleye <haseleye@gmail.com>
2024-11-03 23:09:41 +0200

    Update users.js

--- models/users.js
+++ models/users.js
@@ -224,6 +224,7 @@ userSchema.pre('save', function(next) {
             if (err) return next(err);
 
             // override the cleartext password with the hashed one
+            console.log(hash)
             console.log(`Password: ${user.password}`)
             user.password = hash;
             console.log(`Hashed Password: ${user.password}`)
